2dc28c5053354e2e9f54073be28b96c7af233c84
andy_pl <andrzejw.baranowski@gmail.com>
2018-01-29 19:13:48 +0100

    Added score display method.

--- app/src/main/java/com/example/android/soccercounter/MainActivity.java
+++ app/src/main/java/com/example/android/soccercounter/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.android.soccercounter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    /**
+     * Displays the given score for Teams.
+     */
+    public void displayScore(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.score);
+        scoreView.setText(String.valueOf(score));
     }
 }
